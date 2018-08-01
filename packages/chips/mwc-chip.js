a81718505a9e8f4d62932d275782f3451e9261c2
Ionescu Marian <i.marian97@gmail.com>
2018-08-01 15:41:31 +0300

    ChipSet filter checkmark
    Add support for ChipSet filter checkmark with optional leading icon

--- packages/chips/mwc-chip.js
+++ packages/chips/mwc-chip.js
@@ -62,6 +62,12 @@ export class Chip extends ComponentElement {
       <div class$="mdc-chip ${this.active ? 'mdc-chip--activated' : ''}"
         on-MDCChip:interaction="${this._boundInteractionHandler}">
         ${leadingIconPart}
+        <div class="mdc-chip__checkmark" >
+          <svg class="mdc-chip__checkmark-svg" viewBox="-2 -3 30 30">
+            <path class="mdc-chip__checkmark-path" fill="none" stroke="black"
+                  d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+          </svg>
+        </div>
         <div class="mdc-chip__text">${label}</div>
         ${trailingIconPart}
       </div>`;
