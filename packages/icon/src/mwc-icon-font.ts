b8b6069538034acefcb802affa0b25dc19f04039
Daniel Freedman <dfreedm@google.com>
2018-10-02 17:44:04 -0700

    Update dependencies
    - Typescript 3.1
      - Fix radio, drawer, icon-font, and ripple for new DOM typings
    - node-sass -> sass

--- packages/icon/src/mwc-icon-font.ts
+++ packages/icon/src/mwc-icon-font.ts
@@ -19,4 +19,4 @@ limitations under the License.
 const fontEl = document.createElement('link');
 fontEl.rel = 'stylesheet';
 fontEl.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
-document.head.appendChild(fontEl);
+document.head!.appendChild(fontEl);
