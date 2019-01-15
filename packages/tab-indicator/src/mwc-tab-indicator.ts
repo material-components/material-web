05e8fcf4f94192aac2b194817cf675af39762bae
Giorgio Garasto <giorgio@garasto.it>
2019-01-15 08:55:00 +0100

    Use static styles on mwc-tab-indicator

--- packages/tab-indicator/src/mwc-tab-indicator.ts
+++ packages/tab-indicator/src/mwc-tab-indicator.ts
@@ -59,8 +59,8 @@ export class TabIndicator extends BaseElement {
   @property({type: Boolean})
   fade = false;
 
-  renderStyle() {
-    return style;
+  static get styles() {
+    return [style];
   }
 
   render() {
@@ -70,7 +70,6 @@ export class TabIndicator extends BaseElement {
       'mdc-tab-indicator__content--underline': !this.icon
     };
     return html`
-      ${this.renderStyle()}
       <span class="mdc-tab-indicator ${classMap({'mdc-tab-indicator--fade': this.fade})}">
         <span class="mdc-tab-indicator__content ${classMap(contentClasses)}">${this.icon}</span>
       </span>
