65fabe0bac4036f9f02dc079aea66178b0bba10f
Frankie Fu <ffu@google.com>
2019-04-09 15:55:41 -0700

    fix #153

--- packages/checkbox/src/mwc-checkbox.ts
+++ packages/checkbox/src/mwc-checkbox.ts
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import {html, FormElement, customElement, property, query, HTMLElementWithRipple, addHasRemoveClass, RippleSurface} from '@material/mwc-base/form-element.js';
+import {html, FormElement, customElement, property, query, observer, HTMLElementWithRipple, addHasRemoveClass, RippleSurface} from '@material/mwc-base/form-element.js';
 import {style} from './mwc-checkbox-css.js';
 import {ripple} from '@material/mwc-ripple/ripple-directive.js';
 import MDCCheckboxFoundation from '@material/checkbox/foundation.js';
@@ -42,6 +42,9 @@ export class Checkbox extends FormElement {
   indeterminate = false;
 
   @property({type: Boolean})
+  @observer(function(this: Checkbox, value: boolean) {
+    this.mdcFoundation.setDisabled(value);
+  })
   disabled = false;
 
   @property({type: String})
