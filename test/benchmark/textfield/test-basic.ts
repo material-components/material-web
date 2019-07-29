10c7545aabe8e44a37087294828c696c5c3977ba
Elliott Marquez <emarquez@google.com>
2019-07-29 15:16:12 -0700

    make directive the default

--- test/benchmark/textfield/test-basic.ts
+++ test/benchmark/textfield/test-basic.ts
@@ -3,5 +3,5 @@ import { html } from 'lit-html';
 import { measureFixtureCreation } from '../helpers';
 
 measureFixtureCreation(html`
-  <mwc-textfield outlined label="this is my label" value="hello"></mwc-textfield>
-`, {numRenders: 500});
+  <mwc-textfield outlined label="this is my label"></mwc-textfield>
+`);
