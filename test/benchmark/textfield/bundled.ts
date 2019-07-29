ecd41af6d10f6e4b42242696765653bcab22d1bb
Elliott Marquez <emarquez@google.com>
2019-07-29 15:16:12 -0700

    make directive the default

--- test/benchmark/textfield/bundled.ts
+++
@@ -1,7 +0,0 @@
-import '@material/mwc-textfield/mwc-textfield-bundle';
-import { html } from 'lit-html';
-import { measureFixtureCreation } from '../helpers';
-
-measureFixtureCreation(html`
-  <mwc-textfield-bundle outlined label="this is my label" value="hello"></mwc-textfield-bundle>
-`, {numRenders: 500});
