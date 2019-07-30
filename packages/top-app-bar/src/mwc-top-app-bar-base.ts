fb1a05d84d43021f2f86753d282001ab4bce6a37
aomarks <aomarks@google.com>
2019-07-30 13:28:01 -0700

    Replace decorated HTMLSlotElement property for compatibility with TypeScript's emitDecoratorMetadata. With decorator metadata, the constructor is emitted into the runtime, which will cause an "HTMLSlotElement is undefined" error in Edge and IE11.
    PiperOrigin-RevId: 260782839

--- packages/top-app-bar/src/mwc-top-app-bar-base.ts
+++ packages/top-app-bar/src/mwc-top-app-bar-base.ts
@@ -36,8 +36,12 @@ export class TopAppBarBase extends BaseElement {
   @query('.mdc-top-app-bar')
   protected mdcRoot!: HTMLElement;
 
+  // _actionItemsSlot should have type HTMLSlotElement, but when TypeScript's
+  // emitDecoratorMetadata is enabled, the HTMLSlotElement constructor will
+  // be emitted into the runtime, which will cause an "HTMLSlotElement is
+  // undefined" error in browsers that don't define it (e.g. Edge and IE11).
   @query('[name="actionItems"]')
-  private _actionItemsSlot!: HTMLSlotElement;
+  private _actionItemsSlot!: HTMLElement;
 
   @property({reflect: true})
   type: TopAppBarTypes = '';
@@ -106,8 +110,9 @@ export class TopAppBarBase extends BaseElement {
       getViewportScrollY: () => this.scrollTarget instanceof Window ?
           this.scrollTarget.pageYOffset :
           this.scrollTarget.scrollTop,
-      getTotalActionItems: () =>
-          this._actionItemsSlot.assignedNodes({flatten: true}).length,
+      getTotalActionItems: () => (this._actionItemsSlot as HTMLSlotElement)
+                                     .assignedNodes({flatten: true})
+                                     .length,
     };
   }
 
