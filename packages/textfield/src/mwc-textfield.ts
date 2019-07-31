ffc32911917c86a1b0d933a44e82cdce3440d020
Elliott Marquez <emarquez@google.com>
2019-07-31 16:02:34 -0700

    run formatter

--- packages/textfield/src/mwc-textfield.ts
+++ packages/textfield/src/mwc-textfield.ts
@@ -15,8 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {TextFieldBase} from './mwc-textfield-base.js';
 import {customElement} from '@material/mwc-base/form-element.js';
+import {TextFieldBase} from './mwc-textfield-base.js';
 import {style} from './mwc-textfield-css.js';
 
 declare global {
