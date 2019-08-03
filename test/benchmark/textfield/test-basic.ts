2b825e880677e9a31b539d76a44c49f1f0b19837
Elliott Marquez <emarquez@google.com>
2019-08-02 17:31:46 -0700

    benchmarking implementations

--- test/benchmark/textfield/test-basic.ts
+++
@@ -1,7 +0,0 @@
-import '@material/mwc-textfield/mwc-textfield';
-import {html} from 'lit-html';
-import {measureFixtureCreation} from '../helpers';
-
-measureFixtureCreation(html`
-  <mwc-textfield outlined label="this is my label"></mwc-textfield>
-`);
