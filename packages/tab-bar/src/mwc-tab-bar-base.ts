99234b64d9da108c5ef8d89052590c76081cd841
Abdón Rodríguez Davila <a@abdonrd.com>
2019-08-15 19:29:57 +0200

    Remove any

--- packages/tab-bar/src/mwc-tab-bar-base.ts
+++ packages/tab-bar/src/mwc-tab-bar-base.ts
@@ -134,8 +134,7 @@ export class TabBarBase extends BaseElement {
       },
       getFocusedTabIndex: () => {
         const tabElements = this._getTabs();
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const activeElement = (this as any).getRootNode().activeElement;
+        const activeElement = (this.getRootNode() as ShadowRoot).activeElement;
         return tabElements.indexOf(activeElement);
       },
       getIndexOfTabById: (id: string) => {
