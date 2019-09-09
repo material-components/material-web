178401a0d18d0294d9341b7fc4a5a1fee8111bea
Elliott Marquez <emarquez@google.com>
2019-09-09 16:36:16 -0700

    run formatter

--- packages/base/src/form-element.ts
+++ packages/base/src/form-element.ts
@@ -15,8 +15,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {BaseElement} from './base-element';
 import {MDCRippleFoundation} from '@material/ripple/foundation.js'
+
+import {BaseElement} from './base-element';
+
 export * from './base-element';
 
 export interface RippleSurface extends MDCRippleFoundation {
