5a286bdc7702411f786efa10a39389027f56c870
Alexander Marks <aomarks@google.com>
2019-09-24 10:29:50 -0700

    Move unit test files into package directories.
    Includes splitting tabs test into 3 files.

--- packages/tab-bar/src/test/mwc-tab-bar.test.ts
+++ packages/tab-bar/src/test/mwc-tab-bar.test.ts
@@ -14,27 +14,9 @@
  * limitations under the License.
  */
 
-import {Tab} from '@material/mwc-tab';
 import {TabBar} from '@material/mwc-tab-bar/mwc-tab-bar.js';
-import {TabScroller} from '@material/mwc-tab-scroller/mwc-tab-scroller.js';
 
 
-suite('mwc-tab', () => {
-  let element: Tab;
-
-  setup(() => {
-    element = document.createElement('mwc-tab');
-    document.body.appendChild(element);
-  });
-
-  teardown(() => {
-    document.body.removeChild(element);
-  });
-
-  test('initializes as an mwc-tab', () => {
-    assert.instanceOf(element, Tab);
-  });
-});
 suite('mwc-tab-bar', () => {
   let element: TabBar;
   setup(() => {
@@ -48,22 +30,5 @@ suite('mwc-tab-bar', () => {
 
   test('initializes as an mwc-tab-bar', () => {
     assert.instanceOf(element, TabBar);
-  });
-});
-
-
-suite('mwc-tab-scroller', () => {
-  let element: TabScroller;
-  setup(() => {
-    element = document.createElement('mwc-tab-scroller');
-    document.body.appendChild(element);
-  });
-
-  teardown(() => {
-    document.body.removeChild(element);
-  });
-
-  test('initializes as an mwc-tab-scroller', () => {
-    assert.instanceOf(element, TabScroller);
   });
 });
