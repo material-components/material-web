6bbc44196f398678793ddcaf9978b94b3abb65f4
Elliott Marquez <emarquez@google.com>
2019-12-18 15:57:15 -0800

    remove es6 from demos

--- demos/drawer/drawer.js
+++ demos/drawer/drawer.js
@@ -1,5 +1,5 @@
-const drawer = document.getElementsByTagName('mwc-drawer')[0];
-const container = drawer.parentNode;
-container.addEventListener('MDCTopAppBar:nav', (e) => {
+var drawer = document.getElementsByTagName('mwc-drawer')[0];
+var container = drawer.parentNode;
+container.addEventListener('MDCTopAppBar:nav', function(e) {
   drawer.open = !drawer.open;
 });
