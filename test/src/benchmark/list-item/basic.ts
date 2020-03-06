405840aa98edd7323a8d07d721e3107f2acece66
Elliott Marquez <emarquez@google.com>
2020-03-06 15:24:08 -0800

    perf(list): benchmarks for list and list-item

--- test/src/benchmark/list-item/basic.ts
+++ test/src/benchmark/list-item/basic.ts
@@ -16,11 +16,11 @@
  */
 import '@material/mwc-list/mwc-list-item';
 
+import {measureFixtureCreation} from '../../util/helpers';
 import {html} from 'lit-html';
 
-import {measureFixtureCreation} from '../../util/helpers';
-
 measureFixtureCreation(
-    html`<mwc-list-item selected activated>Apple</mwc-list-item>`, {
-      numRenders: 100,
-    });
+  html`<mwc-list-item selected activated>Apple</mwc-list-item>`, {
+    numRenders: 100,
+  });
+
