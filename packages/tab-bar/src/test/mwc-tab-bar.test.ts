3755e6b41416a133bd66aa12ac077aa97decd1ab
43081j <43081j@users.noreply.github.com>
2020-03-28 20:43:07 +0000

    test: add tab bar tests

--- packages/tab-bar/src/test/mwc-tab-bar.test.ts
+++ packages/tab-bar/src/test/mwc-tab-bar.test.ts
@@ -15,21 +15,81 @@
  * limitations under the License.
  */
 
-import {TabBar} from '@material/mwc-tab-bar/mwc-tab-bar.js';
+import '@material/mwc-tab';
 
+import {TabBar} from '@material/mwc-tab-bar/mwc-tab-bar.js';
+import {html, TemplateResult} from 'lit-html';
+
+import {fixture, TestFixture} from '../../../../test/src/util/helpers';
+
+interface TabBarProps {
+  activeIndex: number;
+  contents: TemplateResult;
+}
+
+const defaultTabBar = html`<mwc-tab-bar></mwc-tab-bar>`;
+
+const tabBar = (propsInit: Partial<TabBarProps>) => {
+  return html`
+    <mwc-tab-bar
+      .activeIndex=${propsInit.activeIndex ?? 0}>
+      ${propsInit.contents ?? html``}
+    </mwc-tab-bar>
+  `;
+};
 
 suite('mwc-tab-bar', () => {
+  let fixt: TestFixture;
   let element: TabBar;
-  setup(() => {
-    element = document.createElement('mwc-tab-bar');
-    document.body.appendChild(element);
-  });
 
   teardown(() => {
-    document.body.removeChild(element);
+    fixt.remove();
   });
 
-  test('initializes as an mwc-tab-bar', () => {
-    assert.instanceOf(element, TabBar);
+  suite('basic', () => {
+    setup(async () => {
+      fixt = await fixture(defaultTabBar);
+      element = fixt.root.querySelector('mwc-tab-bar')!;
+    });
+
+    test('initializes as an mwc-tab-bar', () => {
+      assert.instanceOf(element, TabBar);
+      assert.equal(element.activeIndex, 0);
+    });
+  });
+
+  suite('activation', () => {
+    setup(async () => {
+      fixt = await fixture(tabBar({
+        contents: html`
+          <mwc-tab label="one"></mwc-tab>
+          <mwc-tab label="two"></mwc-tab>
+        `
+      }));
+      element = fixt.root.querySelector('mwc-tab-bar')!;
+      await element.updateComplete;
+    });
+
+    test('clicking tab activates it', async () => {
+      const tab1 = element.children[0] as HTMLElement;
+      const tab2 = element.children[1] as HTMLElement;
+      const tab1Button = tab1.shadowRoot!.querySelector('button')!;
+      const tab2Button = tab2.shadowRoot!.querySelector('button')!;
+
+      tab2Button.click();
+      assert.equal(element.activeIndex, 1);
+      tab1Button.click();
+      assert.equal(element.activeIndex, 0);
+    });
+
+    test('responds to key navigation', async () => {
+      const bar = element.shadowRoot!.querySelector('.mdc-tab-bar')!;
+      const tab1 = element.children[0] as HTMLElement;
+      const tab2 = element.children[1] as HTMLElement;
+      bar.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowRight'}));
+      assert.equal(fixt.root.activeElement, tab2);
+      bar.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowLeft'}));
+      assert.equal(fixt.root.activeElement, tab1);
+    });
   });
 });
