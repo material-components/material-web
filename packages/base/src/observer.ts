3a44d36b47b9362b39090ebfef7e133245dfd7a5
Material Web Team <material-web-copybara@google.com>
2020-07-09 17:06:01 -0700

    Improves typing for the @observer decorator.
    PiperOrigin-RevId: 320505251

--- packages/base/src/observer.ts
+++ packages/base/src/observer.ts
@@ -13,27 +13,47 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
-import {PropertyValues} from 'lit-element/lib/updating-element';
+*/// tslint:disable:no-any
 
+import {PropertyValues, UpdatingElement} from 'lit-element/lib/updating-element';
+
+/**
+ * Observer function type.
+ */
 export interface Observer {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (value: any, old: any): void;
 }
 
+type UpdatingElementClass = typeof UpdatingElement;
+interface UpdatingElementClassWithObservers extends UpdatingElementClass {
+  // tslint:disable-next-line:enforce-name-casing
+  _observers: Map<PropertyKey, Observer>;
+}
+
+/**
+ * Specifies an observer callback that is run when the decorated property
+ * changes. The observer receives the current and old value as arguments.
+ */
 export const observer = (observer: Observer) =>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (proto: any, propName: PropertyKey) => {
       // if we haven't wrapped `updated` in this class, do so
-      if (!proto.constructor._observers) {
+      if (!(proto.constructor as UpdatingElementClassWithObservers)
+               ._observers) {
         proto.constructor._observers = new Map<PropertyKey, Observer>();
         const userUpdated = proto.updated;
-        proto.updated = function(changedProperties: PropertyValues) {
+        proto.updated = function(
+            this: UpdatingElement, changedProperties: PropertyValues) {
           userUpdated.call(this, changedProperties);
           changedProperties.forEach((v, k) => {
-            const observer = this.constructor._observers.get(k);
+            const observers =
+                (this.constructor as UpdatingElementClassWithObservers)
+                    ._observers;
+            const observer = observers.get(k);
             if (observer !== undefined) {
-              observer.call(this, this[k], v);
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              observer.call(this, (this as any)[k], v);
             }
           });
         };
