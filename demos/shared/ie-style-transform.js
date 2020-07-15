80c581609cf4890049ca16b47edf16a0651a27a8
Elliott Marquez <emarquez@google.com>
2020-07-15 14:06:09 -0400

    fix(demos): styles on FF

--- demos/shared/ie-style-transform.js
+++
@@ -1,8 +0,0 @@
-const styles = document.head.querySelectorAll('link[rel="preload"][as="style"]');
-
-for (let i = 0; i < styles.length; i++) {
-  const style = styles[i];
-  style.removeAttribute('onload');
-  style.setAttribute('rel', 'stylesheet');
-  style.removeAttribute('as');
-}
