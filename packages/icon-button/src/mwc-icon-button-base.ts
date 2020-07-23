c9e8a848b8dbd43fa50cd050fa3405343d386fc9
Elliott Marquez <emarquez@google.com>
2020-07-22 18:06:37 -0700

    Rollback of #1498
    breaking icon-button-toggle
    
    PiperOrigin-RevId: 322695808

--- packages/icon-button/src/mwc-icon-button-base.ts
+++ packages/icon-button/src/mwc-icon-button-base.ts
@@ -14,11 +14,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import '@material/mwc-ripple/mwc-ripple';
-
-import {Ripple} from '@material/mwc-ripple/mwc-ripple';
-import {RippleHandlers} from '@material/mwc-ripple/ripple-handlers';
-import {eventOptions, html, internalProperty, LitElement, property, query, queryAsync} from 'lit-element';
+import {ripple} from '@material/mwc-ripple/ripple-directive.js';
+import {html, LitElement, property} from 'lit-element';
 
 /** @soyCompatible */
 export class IconButtonBase extends LitElement {
@@ -28,98 +25,15 @@ export class IconButtonBase extends LitElement {
 
   @property({type: String}) label = '';
 
-  @query('button') buttonElement!: HTMLElement;
-
-  @queryAsync('mwc-ripple') ripple!: Promise<Ripple|null>;
-
-  @internalProperty() protected shouldRenderRipple = false;
-
-  protected rippleHandlers: RippleHandlers = new RippleHandlers(() => {
-    this.shouldRenderRipple = true;
-    return this.ripple;
-  });
-
-  /** @soyCompatible */
-  protected renderRipple() {
-    return html`${
-        this.shouldRenderRipple ? html`
-            <mwc-ripple
-                .disabled="${this.disabled}"
-                unbounded>
-            </mwc-ripple>` :
-                                  ''}`;
-  }
-
-  focus() {
-    const buttonElement = this.buttonElement;
-    if (buttonElement) {
-      this.rippleHandlers.startFocus();
-      buttonElement.focus();
-    }
-  }
-
-  blur() {
-    const buttonElement = this.buttonElement;
-    if (buttonElement) {
-      this.rippleHandlers.endFocus();
-      buttonElement.blur();
-    }
-  }
-
   /** @soyCompatible */
   protected render() {
     return html`<button
-        class="mdc-icon-button"
-        aria-label="${this.label || this.icon}"
-        ?disabled="${this.disabled}"
-        @focus="${this.handleRippleFocus}"
-        @blur="${this.handleRippleBlur}"
-        @mousedown="${this.handleRippleMouseDown}"
-        @mouseenter="${this.handleRippleMouseEnter}"
-        @mouseleave="${this.handleRippleMouseLeave}"
-        @touchstart="${this.handleRippleTouchStart}"
-        @touchend="${this.handleRippleDeactivate}"
-        @touchcancel="${this.handleRippleDeactivate}">
-      ${this.renderRipple()}
+    .ripple="${ripple()}"
+    class="mdc-icon-button"
+    aria-label="${this.label || this.icon}"
+    ?disabled="${this.disabled}">
     <i class="material-icons">${this.icon}</i>
     <slot></slot>
   </button>`;
-  }
-
-  @eventOptions({passive: true})
-  protected handleRippleMouseDown(event?: Event) {
-    const onUp = () => {
-      window.removeEventListener('mouseup', onUp);
-
-      this.handleRippleDeactivate();
-    };
-
-    window.addEventListener('mouseup', onUp);
-    this.rippleHandlers.startPress(event);
-  }
-
-  @eventOptions({passive: true})
-  protected handleRippleTouchStart(event?: Event) {
-    this.rippleHandlers.startPress(event);
-  }
-
-  protected handleRippleDeactivate() {
-    this.rippleHandlers.endPress();
-  }
-
-  protected handleRippleMouseEnter() {
-    this.rippleHandlers.startHover();
-  }
-
-  protected handleRippleMouseLeave() {
-    this.rippleHandlers.endHover();
-  }
-
-  protected handleRippleFocus() {
-    this.rippleHandlers.startFocus();
-  }
-
-  protected handleRippleBlur() {
-    this.rippleHandlers.endFocus();
   }
 }
