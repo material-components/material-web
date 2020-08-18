8604d59edb9f29fe467f6a31baeb39f5aba00679
Liz Mitchell <asyncliz@gmail.com>
2020-08-17 17:59:35 -0700

    chore: remove src folder from packages in GitHub (#1706)
    * chore: remove src folder from packages in GitHub
    
    * chore: add extra newline to button sass

--- scripts/build-styling.sh
+++ scripts/build-styling.sh
@@ -26,11 +26,6 @@ for sassfile in ${sassfiles[@]}; do
     continue
   fi
   cssts=`echo ${sassfile} | sed -e 's/.scss/-css.ts/'`
-  lastdir=`basename $(dirname ${cssts})`
-  # skip sass files outside of src folders
-  if [[ ${lastdir} != "src" ]]; then
-    continue
-  fi
   echo "Generating ${cssts}"
   node scripts/sass-render/bin/sass-render.js -t sass-template.tmpl -s ${sassfile} -o ${cssts}
 done
