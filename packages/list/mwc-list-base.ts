5de00c1b2a6baa43d5f05ca74b98093c31af927e
Elliott Marquez <emarquez@google.com>
2021-01-27 12:11:39 -0800

    fix(list): fix list unpkg
    PiperOrigin-RevId: 354143157

--- packages/list/mwc-list-base.ts
+++ packages/list/mwc-list-base.ts
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import '@material/mwc-list/mwc-list-item';
+import './mwc-list-item';
 
 import {BaseElement} from '@material/mwc-base/base-element';
 import {observer} from '@material/mwc-base/observer';
