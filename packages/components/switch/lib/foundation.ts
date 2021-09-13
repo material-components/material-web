78abbd59c73588f06c08a28ec0ee0a474cda2d01
Material Web Team <material-web-copybara@google.com>
2021-09-13 14:22:48 -0700

    feat(switch, m3): migrate switch state, foundation, and scss files
    PiperOrigin-RevId: 396442605

---
+++ packages/components/switch/lib/foundation.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {MDCObserverFoundation} from '@material/base/observer-foundation.js';
+
+import {MDCSwitchState} from './state.js';
+
+/**
+ * `MDCSwitchFoundation` provides a state-only foundation for a switch
+ * component.
+ *
+ * State observers and event handler entrypoints update a component's adapter's
+ * state with the logic needed for switch to function.
+ */
+export class MDCSwitchFoundation extends MDCObserverFoundation<MDCSwitchState> {
+  constructor(adapter: MDCSwitchState) {
+    super(adapter);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  /**
+   * Initializes the foundation and starts observing state changes.
+   */
+  override init() {
+    this.observe(this.adapter, {
+      disabled: this.stopProcessingIfDisabled,
+      processing: this.stopProcessingIfDisabled,
+    });
+  }
+
+  /**
+   * Event handler for switch click events. Clicking on a switch will toggle its
+   * selected state.
+   */
+  handleClick() {
+    if (this.adapter.disabled) {
+      return;
+    }
+
+    this.adapter.selected = !this.adapter.selected;
+  }
+
+  protected stopProcessingIfDisabled() {
+    if (this.adapter.disabled) {
+      this.adapter.processing = false;
+    }
+  }
+}
---
+++ packages/components/switch/lib/state.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * The state of the switch.
+ */
+export interface MDCSwitchState {
+  /**
+   * Indicates whether or not the switch is disabled.
+   */
+  disabled: boolean;
+  /**
+   * Indicates whether or not the switch is processing and showing a loading
+   * indicator. A disabled switch cannot be processing.
+   */
+  processing: boolean;
+  /**
+   * If true, the switch is on. If false, the switch is off.
+   */
+  selected: boolean;
+}
