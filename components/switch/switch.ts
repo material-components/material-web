3dbb73416fb11b6e7833e59ba70ef8f8eaccd9a5
Material Web Team <material-web-copybara@google.com>
2021-09-24 11:31:43 -0700

    feat(switch, m3): migrate switch component, create demo
    PiperOrigin-RevId: 398776037

---
+++ components/switch/switch.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+import {customElement} from 'lit/decorators';
+
+import {styles} from './lib/switch-styles.css.js'
+import {Switch} from './lib/switch.js';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-switch': MDSwitch;
+  }
+}
+
+@customElement('md-switch')
+export class MDSwitch extends Switch {
+  static styles = [styles];
+}
