5454d7897c96d37a67248616d5710ad36b03ddb3
Liz Mitchell <lizmitchell@google.com>
2021-11-05 15:16:00 -0700

    feat(motion): add animation Sass and TS constants
    PiperOrigin-RevId: 407923407

---
+++ components/motion/animation.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export enum Easing {
+  STANDARD = 'cubic-bezier(0.4, 0, 0.2, 1)',
+  ACCELERATION = 'cubic-bezier(0.4, 0, 1, 1)',
+  DECELERATION = 'cubic-bezier(0, 0, 0.2, 1)',
+  SHARP = 'cubic-bezier(0.4, 0, 0.6, 1)',
+}
