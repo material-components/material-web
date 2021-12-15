5b222d340260043ad3ac13ae74f69844e3909573
Material Web Team <material-web-copybara@google.com>
2021-12-14 22:29:04 -0800

    feat(navigation-drawer): add navigation drawer to the m3 components dir
    PiperOrigin-RevId: 416471849

---
+++ components/navigation-drawer/lib/navigation-drawer.ts
@@ -0,0 +1,57 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {html, LitElement, TemplateResult} from 'lit';
+import {property} from 'lit/decorators';
+import {classMap} from 'lit/directives/class-map';
+import {ifDefined} from 'lit/directives/if-defined';
+
+/** @soyCompatible */
+export class NavigationDrawer extends LitElement {
+  /* aria properties */
+  @property({attribute: 'aria-describedby', type: String})
+  ariaDescribedBy: string|undefined;
+  @property({attribute: 'aria-label', type: String})
+  override ariaLabel: string = 'navigational drawer';
+  @property({attribute: 'aria-modal', type: String})
+  override ariaModal: 'true'|'false' = 'false';
+  @property({attribute: 'aria-labelledby', type: String})
+  ariaLabelledBy: string|undefined;
+
+  @property({type: Boolean}) opened = false;
+  @property({type: String}) pivot: 'start'|'end' = 'end';
+
+  /** @soyTemplate */
+  override render(): TemplateResult {
+    const ariaExpanded = this.opened ? 'true' : 'false';
+    const ariaHidden = !this.opened ? 'true' : 'false';
+    const frameClasses = this.getFrameClassMap();
+
+    return html`
+      <div
+        aria-describedby="${ifDefined(this.ariaDescribedBy)}"
+        aria-expanded="${ariaExpanded}"
+        aria-hidden="${ariaHidden}"
+        aria-label="${this.ariaLabel}"
+        aria-labelledby="${ifDefined(this.ariaLabelledBy)}"
+        aria-modal="${this.ariaModal}"
+        class="md3-navigation-drawer__frame ${frameClasses}"
+        role="dialog">
+        <div class="md3-navigation-drawer__slot-content">
+          <slot></slot>
+        </div>
+      </div>
+    `;
+  }
+
+  /** @soyTemplate classMap */
+  protected getFrameClassMap() {
+    return classMap({
+      'md3-navigation-drawer__closed-to-opened': this.opened,
+      'md3-navigation-drawer__pivot-at-start': this.pivot === 'start',
+    });
+  }
+}
