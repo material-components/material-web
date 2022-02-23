19b6ee9ec42e31dcdb06d5c6e71b56d578f5abae
Joy Zhong <joyzhong@google.com>
2022-02-23 10:45:49 -0800

    feat(icon): Fork icon package to m3/.
    PiperOrigin-RevId: 430489708

---
+++ components/icon/icon.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {customElement} from 'lit/decorators';
+
+import {Icon} from './lib/icon';
+import {styles} from './lib/icon-styles.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-icon': MdIcon;
+  }
+}
+
+@customElement('md-icon')
+export class MdIcon extends Icon {
+  static override styles = [styles];
+}
