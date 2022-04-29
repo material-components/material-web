5897f3aed55dbf400eb4f753991345883031b646
Material Web Team <material-web-copybara@google.com>
2022-04-29 10:58:54 -0700

    refactor(focus-ring): focus ring now wraps the component it focuses.
    PiperOrigin-RevId: 445459202

--- focus/lib/focus-ring.ts
+++ focus/lib/focus-ring.ts
@@ -5,8 +5,11 @@
  */
 
 import {html, LitElement, TemplateResult} from 'lit';
-import {property} from 'lit/decorators';
+import {eventOptions, property} from 'lit/decorators';
 import {ClassInfo, classMap} from 'lit/directives/class-map';
+
+// TODO: Should strong-focus be moved into /lib?
+import {pointerPress, shouldShowStrongFocus} from '../strong-focus';
 
 /** @soyCompatible */
 export class FocusRing extends LitElement {
@@ -14,8 +17,16 @@ export class FocusRing extends LitElement {
 
   /** @soyTemplate */
   protected override render(): TemplateResult {
-    return html`<span class="md3-focus-ring ${
-        classMap(this.getRenderClasses())}"></span>`;
+    return html`
+    <span
+        class="md3-focus-ring__container"
+        @focusin="${this.handleFocus}"
+        @focusout="${this.handleBlur}"
+        @pointerdown="${this.handlePointerDown}">
+      <span class="md3-focus-ring ${classMap(this.getRenderClasses())}"></span>
+      <span><slot></slot></span>
+    </span>
+    `;
   }
 
   /** @soyTemplate */
@@ -23,5 +34,18 @@ export class FocusRing extends LitElement {
     return {
       'md3-focus-ring--visible': this.visible,
     };
+  }
+
+  protected handleFocus() {
+    this.visible = shouldShowStrongFocus();
+  }
+
+  protected handleBlur() {
+    this.visible = false;
+  }
+
+  @eventOptions({passive: true})
+  protected handlePointerDown(event: PointerEvent) {
+    pointerPress();
   }
 }
