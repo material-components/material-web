261b6efc0c56768abc233b48ca1a2591eeea0fcc
Material Web Team <material-web-copybara@google.com>
2022-07-19 10:06:04 -0700

    feat(list): Added video web component to list
    PiperOrigin-RevId: 461910693

---
+++ list/lib/video/list-item-video.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {html, LitElement, TemplateResult} from 'lit';
+import {property} from 'lit/decorators';
+import {ifDefined} from 'lit/directives/if-defined';
+
+/** @soyCompatible */
+export class ListItemVideo extends LitElement {
+  @property({type: String, reflect: true}) media = 'video';
+  @property({type: String, reflect: true}) video = '';
+  @property({type: String, reflect: true}) altText?: string;
+
+  /** @soyTemplate */
+  override render(): TemplateResult {
+    return html`
+        <img src="${this.video}" alt="${ifDefined(this.altText)}"
+         class="md3-list-item__video" />
+      `;
+  }
+}
---
+++ list/list-item-video.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {customElement} from 'lit/decorators';
+
+import {ListItemVideo} from './lib/video/list-item-video';
+import {styles} from './lib/video/list-item-video-styles.css';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-list-item-video': MdListItemVideo;
+  }
+}
+
+/**
+ * @soyCompatible
+ * @final
+ * @suppress {visibility}
+ */
+@customElement('md-list-item-video')
+export class MdListItemVideo extends ListItemVideo {
+  static override styles = [styles];
+}
