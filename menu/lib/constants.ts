de2993744d6f83850d0c80cf56a9ff245cbc4168
Joy Zhong <joyzhong@google.com>
2022-07-20 10:39:47 -0700

    feat(menu): Add menu foundation/adapter and Sass (forked from MDC).
    PiperOrigin-RevId: 462177572

---
+++ menu/lib/constants.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const cssClasses = {
+  MENU_SELECTED_LIST_ITEM: 'md3-menu-item--selected',
+  MENU_SELECTION_GROUP: 'md3-menu__selection-group',
+  ROOT: 'md3-menu',
+};
+
+const strings = {
+  ARIA_CHECKED_ATTR: 'aria-checked',
+  ARIA_DISABLED_ATTR: 'aria-disabled',
+  CHECKBOX_SELECTOR: 'input[type="checkbox"]',
+  LIST_SELECTOR: '.md3-list,.md3-deprecated-list',
+  SELECTED_EVENT: 'MDCMenu:selected',
+  SKIP_RESTORE_FOCUS: 'data-menu-item-skip-restore-focus',
+};
+
+const numbers = {
+  FOCUS_ROOT_INDEX: -1,
+};
+
+enum DefaultFocusState {
+  NONE = 0,
+  LIST_ROOT = 1,
+  FIRST_ITEM = 2,
+  LAST_ITEM = 3,
+}
+
+export {cssClasses, strings, numbers, DefaultFocusState};
