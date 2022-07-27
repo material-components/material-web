b315d62c50cc7c80b66bc8cca6622839bc5d9da3
Material Web Team <material-web-copybara@google.com>
2022-07-26 17:01:51 -0700

    chore(chips): Fork foundation, adapter, and Sass files: Action
    PiperOrigin-RevId: 463460098

---
+++ chips/action/lib/primary-foundation.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {MDCChipActionAttributes, MDCChipActionType} from './constants';
+import {MDCChipActionFoundation} from './foundation';
+
+/**
+ * MDCChipPrimaryActionFoundation provides the business logic for the primary
+ * chip action.
+ */
+export class MDCChipPrimaryActionFoundation extends MDCChipActionFoundation {
+  isSelectable() {
+    return this.adapter.getAttribute(MDCChipActionAttributes.ROLE) === 'option';
+  }
+
+  actionType() {
+    return MDCChipActionType.PRIMARY;
+  }
+
+  protected shouldEmitInteractionOnRemoveKey() {
+    return this.adapter.getAttribute(MDCChipActionAttributes.DATA_DELETABLE) ===
+        'true';
+  }
+}
