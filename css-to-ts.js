ecf25785696aa0cd6d5197f10f9684c9de3877bc
Elizabeth Mitchell <lizmitchell@google.com>
2022-08-16 08:37:16 -0700

    chore: add package.json with basic build
    PiperOrigin-RevId: 467933458

---
+++ css-to-ts.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const fs = require('fs');
+
+for (let i = 2; i < process.argv.length; i++) {
+  try {
+    const filePath = process.argv[i];
+    const content = fs.readFileSync(filePath);
+    fs.writeFileSync(`${filePath}.ts`, `/**
+  * @license
+  * Copyright 2022 Google LLC
+  * SPDX-License-Identifier: Apache-2.0
+  */
+ import {css} from 'lit';
+ export const styles = css\`${content.toString('utf8')}\`;
+ `);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
