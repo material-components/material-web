dc8045e5687305e113954c283a004e5d412cd61d
Material Web Team <material-web-copybara@google.com>
2022-08-16 22:33:43 -0700

    chore(chips): Fork foundation, adapter, and Sass files: ChipSet
    PiperOrigin-RevId: 468108061

---
+++ chips/chipset/lib/constants.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * MDCChipSetAttributes provides the named constants for attributes used by the
+ * foundation.
+ */
+export enum MDCChipSetAttributes {
+  ARIA_MULTISELECTABLE = 'aria-multiselectable',
+}
+
+/**
+ * MDCChipSetCssClasses provides the named constants for class names.
+ */
+export enum MDCChipSetCssClasses {
+  CHIP = 'md3-chip',
+}
+
+/**
+ * MDCChipSetEvents provides the constants for emitted events.
+ */
+export enum MDCChipSetEvents {
+  INTERACTION = 'MDCChipSet:interaction',
+  REMOVAL = 'MDCChipSet:removal',
+  SELECTION = 'MDCChipSet:selection',
+}
