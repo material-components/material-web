7c6ea4ad9d9978cf269b7e602fbd58f89ce84859
Elizabeth Mitchell <lizmitchell@google.com>
2022-10-03 09:44:35 -0700

    chore(navigationtab): remove compat deps
    PiperOrigin-RevId: 478529093

--- navigationtab/md-navigation-tab_test.ts
+++ navigationtab/md-navigation-tab_test.ts
@@ -4,11 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {doesElementContainFocus} from '@material/web/compat/base/utils.js';
-import {fixture, TestFixture} from '@material/web/compat/testing/helpers.js';
-import {MdFocusRing} from '@material/web/focus/focus-ring.js';
-import {html} from 'lit';
+import {html, render} from 'lit';
 import {customElement} from 'lit/decorators.js';
+
+import {Environment} from '../testing/environment.js';
 
 import {NavigationTabHarness} from './harness.js';
 import {MdNavigationTab} from './navigation-tab.js';
@@ -24,107 +23,118 @@ declare global {
 }
 
 describe('mwc-navigation-tab', () => {
-  let fixt: TestFixture;
-  let element: TestNavigationTab;
-  let harness: NavigationTabHarness;
+  const env = new Environment();
 
-  afterEach(() => {
-    fixt.remove();
-  });
+  async function setupTest() {
+    // Variant type does not matter for shared tests
+    const element =
+        env.render(html`<md-test-navigation-tab></md-test-navigation-tab>`)
+            .querySelector('md-test-navigation-tab');
+    if (!element) {
+      throw new Error('Could not query rendered <md-test-navigation-tab>.');
+    }
+
+    await env.waitForStability();
+    const button = element.renderRoot.querySelector('button');
+    if (!button) {
+      throw new Error('Could not query rendered <button>.');
+    }
+
+    const navTab = element.renderRoot.querySelector('.md3-navigation-tab');
+    if (!navTab) {
+      throw new Error('Could not query rendered .md3-navigation-tab.');
+    }
+
+    const focusRing = element.renderRoot.querySelector('md-focus-ring');
+    if (!focusRing) {
+      throw new Error('Could not query rendered <md-focus-ring>.');
+    }
+
+    return {
+      button,
+      navTab,
+      focusRing,
+      harness: new NavigationTabHarness(element),
+    };
+  }
 
   describe('basic', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab></md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      harness = new NavigationTabHarness(element);
-      await element.updateComplete;
-    });
-
-    it('initializes as an md-navigation-tab', () => {
-      expect(element).toBeInstanceOf(MdNavigationTab);
-      expect(element.active).toBeFalse();
-      expect(element.hideInactiveLabel).toBeFalse();
-      expect(element.label).toBeUndefined();
-      expect(element.badgeValue).toEqual('');
-      expect(element.showBadge).toBeFalse();
+    it('initializes as an md-navigation-tab', async () => {
+      const {harness} = await setupTest();
+      expect(harness.element).toBeInstanceOf(MdNavigationTab);
+      expect(harness.element.active).toBeFalse();
+      expect(harness.element.hideInactiveLabel).toBeFalse();
+      expect(harness.element.label).toBeUndefined();
+      expect(harness.element.badgeValue).toEqual('');
+      expect(harness.element.showBadge).toBeFalse();
     });
 
     it('emits interaction event on click', async () => {
+      const {harness} = await setupTest();
       const interactionHandler = jasmine.createSpy();
-      element.addEventListener(
+      harness.element.addEventListener(
           'navigation-tab-interaction', interactionHandler);
       await harness.clickWithMouse();
       expect(interactionHandler).toHaveBeenCalled();
     });
 
-    it('focus() sets focus on button element', () => {
-      const button = element.shadowRoot!.querySelector('button')!;
-      element.focus();
-      expect(doesElementContainFocus(button)).toBeTrue();
+    it('focus() sets focus on button element', async () => {
+      const {harness, button} = await setupTest();
+      harness.element.focus();
+      expect(button.matches(':focus')).toBeTrue();
     });
   });
 
   it('on render navigation-tab-rendered event fires', async () => {
+    const element = document.createElement('md-test-navigation-tab');
     const renderedHandler = jasmine.createSpy();
-    fixt = await fixture(html`
-      <md-test-navigation-tab
-        @navigation-tab-rendered=${() => renderedHandler()}>
-      </md-test-navigation-tab>
-    `);
+    element.addEventListener('navigation-tab-rendered', renderedHandler);
+    env.render(html`${element}`);
+    await env.waitForStability();
     expect(renderedHandler).toHaveBeenCalled();
   });
 
   describe('active', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab
-          ?active=${true}>
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
-
     it('affects `aria-selected` of native button', async () => {
-      const button = element.shadowRoot!.querySelector('button')!;
+      const {harness, button} = await setupTest();
+      harness.element.active = true;
+      await env.waitForStability();
+
       expect(button.getAttribute('aria-selected')).toEqual('true');
 
-      element.active = false;
-      await element.updateComplete;
+      harness.element.active = false;
+      await env.waitForStability();
       expect(button.getAttribute('aria-selected')).toEqual('false');
     });
 
     it('affects `tabindex` of native button', async () => {
-      const button = element.shadowRoot!.querySelector('button')!;
+      const {harness, button} = await setupTest();
+      harness.element.active = true;
+      await env.waitForStability();
+
       expect(button.getAttribute('tabindex')).toEqual('0');
 
-      element.active = false;
-      await element.updateComplete;
+      harness.element.active = false;
+      await env.waitForStability();
       expect(button.getAttribute('tabindex')).toEqual('-1');
     });
 
-    it('sets the correct classes', () => {
-      const navTab = element.shadowRoot!.querySelector('.md3-navigation-tab')!;
+    it('sets the correct classes', async () => {
+      const {harness, navTab} = await setupTest();
+      harness.element.active = true;
+      await env.waitForStability();
+
       expect(navTab.classList.contains('md3-navigation-tab--active'))
           .toBeTrue();
     });
   });
 
   describe('hideInactiveLabel', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab
-          .hideInactiveLabel=${true}>
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
+    it('sets the correct classes', async () => {
+      const {harness, navTab} = await setupTest();
+      harness.element.hideInactiveLabel = true;
+      await env.waitForStability();
 
-    it('sets the correct classes', () => {
-      const navTab = element.shadowRoot!.querySelector('.md3-navigation-tab')!;
       expect(
           navTab.classList.contains('md3-navigation-tab--hide-inactive-label'))
           .toBeTrue();
@@ -132,125 +142,99 @@ describe('mwc-navigation-tab', () => {
   });
 
   describe('label', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab
-          label="foo">
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
+    it('displays label text', async () => {
+      const {harness} = await setupTest();
+      harness.element.label = 'foo';
+      await env.waitForStability();
 
-    it('displays label text', () => {
-      const content =
-          element.shadowRoot!.querySelector('.md3-navigation-tab__label-text')!;
+      const content = harness.element.shadowRoot!.querySelector(
+          '.md3-navigation-tab__label-text')!;
       expect(content.textContent!.trim()).toEqual('foo');
     });
   });
 
   describe('ariaLabel', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab
-          label="foo"
-          aria-label="bar">
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
-
     it('affects `aria-label` of native button', async () => {
-      const button = element.shadowRoot!.querySelector('button')!;
+      const {harness, button} = await setupTest();
+      harness.element.label = 'foo';
+      harness.element.ariaLabel = 'bar';
+      await env.waitForStability();
+
       expect(button.getAttribute('aria-label')).toEqual('bar');
     });
   });
 
   describe('showBadge', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab
-          .showBadge=${true}>
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
+    it('displays badge', async () => {
+      const {harness} = await setupTest();
+      harness.element.showBadge = true;
+      await env.waitForStability();
 
-    it('displays badge', () => {
-      const badge = element.shadowRoot!.querySelector('md-badge')!;
+      const badge = harness.element.renderRoot.querySelector('md-badge');
       expect(badge).toBeDefined();
     });
 
     it('does not display badge if showBadge is false', async () => {
-      element.showBadge = false;
-      await element.updateComplete;
-      const badge = element.shadowRoot!.querySelector('md-badge');
+      const {harness} = await setupTest();
+      harness.element.showBadge = false;
+      await env.waitForStability();
+
+      const badge = harness.element.renderRoot.querySelector('md-badge');
       expect(badge).toEqual(null);
     });
   });
 
   describe('badgeValue', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab
-          .showBadge=${true}
-          .badgeValue=${'9'}>
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
+    it('displays badge value', async () => {
+      const {harness} = await setupTest();
+      harness.element.showBadge = true;
+      harness.element.badgeValue = '9';
+      await env.waitForStability();
 
-    it('displays badge value', () => {
-      const badge = element.shadowRoot!.querySelector('md-badge')!;
-      expect(badge.value).toEqual('9');
+      const badge = harness.element.renderRoot.querySelector('md-badge');
+      expect(badge?.value).toEqual('9');
     });
   });
 
   describe('icons', () => {
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab>
-          <i slot="activeIcon" class="material-icons">star</i>
-          <i slot="inactiveIcon" class="material-icons">star_border</i>
-        </md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      await element.updateComplete;
-    });
+    it('nodes with `slot=activeIcon` will serve as the active icon',
+       async () => {
+         const {harness} = await setupTest();
+         const icons = html`
+           <i slot="activeIcon" class="material-icons">star</i>
+           <i slot="inactiveIcon" class="material-icons">star_border</i>
+         `;
+         render(icons, harness.element);
 
-    it('nodes with `slot=activeIcon` will serve as the active icon', () => {
-      const icon = element.querySelector<HTMLElement>('[slot="activeIcon"]')!;
-      expect(icon.textContent!.trim()).toEqual('star');
-    });
+         const icon =
+             harness.element.querySelector<HTMLElement>('[slot="activeIcon"]')!;
+         expect(icon.textContent!.trim()).toEqual('star');
+       });
 
-    it('nodes with `slot=inactiveIcon` will serve as the inactive icon', () => {
-      const icon = element.querySelector<HTMLElement>('[slot="inactiveIcon"]')!;
-      expect(icon.textContent!.trim()).toEqual('star_border');
-    });
+    it('nodes with `slot=inactiveIcon` will serve as the inactive icon',
+       async () => {
+         const {harness} = await setupTest();
+         const icons = html`
+           <i slot="activeIcon" class="material-icons">star</i>
+           <i slot="inactiveIcon" class="material-icons">star_border</i>
+         `;
+         render(icons, harness.element);
+
+         const icon = harness.element.querySelector<HTMLElement>(
+             '[slot="inactiveIcon"]')!;
+         expect(icon.textContent!.trim()).toEqual('star_border');
+       });
   });
 
   describe('focus ring', () => {
-    let focusRing: MdFocusRing;
-
-    beforeEach(async () => {
-      fixt = await fixture(html`
-        <md-test-navigation-tab></md-test-navigation-tab>
-      `);
-      element = fixt.root.querySelector('md-test-navigation-tab')!;
-      focusRing = element.shadowRoot!.querySelector('md-focus-ring')!;
-      harness = new NavigationTabHarness(element);
-      await element.updateComplete;
-    });
-
     it('hidden on non-keyboard focus', async () => {
+      const {harness, focusRing} = await setupTest();
       await harness.clickWithMouse();
       expect(focusRing.visible).toBeFalse();
     });
 
     it('visible on keyboard focus and hides on blur', async () => {
+      const {harness, focusRing} = await setupTest();
       await harness.focusWithKeyboard();
       expect(focusRing.visible).toBeTrue();
       await harness.blur();
@@ -258,6 +242,7 @@ describe('mwc-navigation-tab', () => {
     });
 
     it('hidden after pointer interaction', async () => {
+      const {harness, focusRing} = await setupTest();
       await harness.focusWithKeyboard();
       expect(focusRing.visible).toBeTrue();
       await harness.clickWithMouse();
