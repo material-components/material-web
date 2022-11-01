fe2eb3aee14e3dabd2233c1cf3c8bd7d048ee246
Elizabeth Mitchell <lizmitchell@google.com>
2022-11-01 09:29:31 -0700

    chore(segmentedbutton): merge static/theme-styles
    PiperOrigin-RevId: 485338234

--- segmentedbutton/outlined-segmented-button.ts
+++ segmentedbutton/outlined-segmented-button.ts
@@ -8,7 +8,7 @@ import {customElement} from 'lit/decorators.js';
 
 import {OutlinedSegmentedButton} from './lib/outlined-segmented-button.js';
 import {styles as outlinedStyles} from './lib/outlined-styles.css.js';
-import {styles as sharedStyles} from './lib/segmented-button-styles.css.js';
+import {styles as sharedStyles} from './lib/shared-styles.css.js';
 
 declare global {
   interface HTMLElementTagNameMap {
