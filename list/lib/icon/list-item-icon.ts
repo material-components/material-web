232982ef034872968924dbb5620f59352c4028c2
Daniel Freedman <dfreedm@google.com>
2023-02-22 14:39:24 -0800

    fix(icon,iconbutton,list)!: use material symbols for icons
    - Material Symbols is recommended for M3
    - Refactor list items to use md-icon directly
    
    PiperOrigin-RevId: 511601359

--- list/lib/icon/list-item-icon.ts
+++ list/lib/icon/list-item-icon.ts
@@ -4,13 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import '../../../icon/icon.js';
+
 import {html, LitElement, TemplateResult} from 'lit';
 
 // tslint:disable-next-line:enforce-comments-on-exported-symbols
 export class ListItemIcon extends LitElement {
   override render(): TemplateResult {
     return html`
-      <span class="md3-list-item__icon"><slot></slot></span>
+      <md-icon class="md3-list-item__icon"><slot></slot></md-icon>
     `;
   }
 }
