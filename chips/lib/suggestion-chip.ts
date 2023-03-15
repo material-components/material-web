f3fe55ec2bcc7dd3ae57f62b3dd68fbc951e747c
Elizabeth Mitchell <lizmitchell@google.com>
2023-03-15 11:16:37 -0700

    feat(chips): add suggestion chips
    PiperOrigin-RevId: 516878268

---
+++ chips/lib/suggestion-chip.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {Chip} from './chip.js';
+
+/**
+ * A suggestion chip component.
+ */
+export class SuggestionChip extends Chip {}
---
+++ chips/suggestion-chip.ts
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {customElement} from 'lit/decorators.js';
+
+import {styles as sharedStyles} from './lib/shared-styles.css.js';
+import {SuggestionChip} from './lib/suggestion-chip.js';
+import {styles} from './lib/suggestion-styles.css.js';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'md-suggestion-chip': MdSuggestionChip;
+  }
+}
+
+/**
+ * TODO(b/243982145): add docs
+ *
+ * @final
+ * @suppress {visibility}
+ */
+@customElement('md-suggestion-chip')
+export class MdSuggestionChip extends SuggestionChip {
+  static override styles = [sharedStyles, styles];
+}
