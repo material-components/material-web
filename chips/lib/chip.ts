9eb861fc835b81511652c6fd2d47aebc9854176d
Elizabeth Mitchell <lizmitchell@google.com>
2023-03-16 10:14:08 -0700

    feat(chips): add focus ring
    PiperOrigin-RevId: 517156213

--- chips/lib/chip.ts
+++ chips/lib/chip.ts
@@ -5,11 +5,14 @@
  */
 
 import '../../elevation/elevation.js';
+import '../../focus/focus-ring.js';
 
 import {LitElement, nothing} from 'lit';
-import {property} from 'lit/decorators.js';
+import {property, state} from 'lit/decorators.js';
 import {classMap} from 'lit/directives/class-map.js';
 import {html as staticHtml, literal} from 'lit/static-html.js';
+
+import {pointerPress, shouldShowStrongFocus} from '../../focus/strong-focus.js';
 
 /**
  * A chip component.
@@ -20,6 +23,8 @@ export class Chip extends LitElement {
   @property({type: String}) href = '';
   @property({type: String}) label = '';
   @property({type: String}) target = '';
+
+  @state() private showFocusRing = false;
 
   override render() {
     const classes = {
@@ -32,10 +37,27 @@ export class Chip extends LitElement {
       <${button} class="container ${classMap(classes)}"
           ?disabled=${this.disabled}
           href=${this.href || nothing}
-          target=${this.href ? this.target : nothing}>
+          target=${this.href ? this.target : nothing}
+          @blur=${this.handleBlur}
+          @focus=${this.handleFocus}
+          @pointerdown=${this.handlePointerDown}>
         <md-elevation shadow=${this.elevated} surface></md-elevation>
+        <md-focus-ring .visible=${this.showFocusRing}></md-focus-ring>
         <div class="label">${this.label}</div>
       </${button}>
     `;
+  }
+
+  private handleBlur() {
+    this.showFocusRing = false;
+  }
+
+  private handleFocus() {
+    this.showFocusRing = shouldShowStrongFocus();
+  }
+
+  private handlePointerDown() {
+    pointerPress();
+    this.showFocusRing = shouldShowStrongFocus();
   }
 }
