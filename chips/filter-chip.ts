095355d33aa7136d779b6a8a39678d562c6501c6
Elizabeth Mitchell <lizmitchell@google.com>
2023-05-09 15:57:33 -0700

    chore(chips): split out trailing icon styles
    PiperOrigin-RevId: 530736697

--- chips/filter-chip.ts
+++ chips/filter-chip.ts
@@ -10,6 +10,7 @@ import {styles as elevatedStyles} from './lib/elevated-styles.css.js';
 import {FilterChip} from './lib/filter-chip.js';
 import {styles} from './lib/filter-styles.css.js';
 import {styles as sharedStyles} from './lib/shared-styles.css.js';
+import {styles as trailingIconStyles} from './lib/trailing-icon-styles.css.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -25,5 +26,6 @@ declare global {
  */
 @customElement('md-filter-chip')
 export class MdFilterChip extends FilterChip {
-  static override styles = [sharedStyles, elevatedStyles, styles];
+  static override styles =
+      [sharedStyles, elevatedStyles, trailingIconStyles, styles];
 }
