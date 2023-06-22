4ec6d4ef9da30ddf12386099c2b50f44c59f8907
Elizabeth Mitchell <lizmitchell@google.com>
2023-06-22 14:01:03 -0700

    test(chips): harness will use trailing action if there isn't a primary
    PiperOrigin-RevId: 542661036

--- chips/harness.ts
+++ chips/harness.ts
@@ -16,10 +16,17 @@ export class ChipHarness extends Harness<Chip> {
 
   protected override async getInteractiveElement() {
     await this.element.updateComplete;
-    if (this.action === 'trailing') {
-      // Retrieve MultiActionChip's trailingAction
-      const {trailingAction} =
-          this.element as {trailingAction?: HTMLElement | null};
+    const {primaryId} = this.element as unknown as {primaryId: string};
+    const primaryAction = primaryId &&
+        this.element.renderRoot.querySelector<HTMLElement>(`#${primaryId}`);
+    // Retrieve MultiActionChip's trailingAction
+    const {trailingAction} =
+        this.element as {trailingAction?: HTMLElement | null};
+
+    // Default to trailing action if there isn't a primary action and the user
+    // didn't explicitly set `harness.action = 'trailing'` (remove-only input
+    // chips).
+    if (this.action === 'trailing' || !primaryAction) {
       if (!trailingAction) {
         throw new Error(
             '`ChipHarness.action` is "trailing", but the chip does not have a trailing action.');
@@ -28,9 +35,6 @@ export class ChipHarness extends Harness<Chip> {
       return trailingAction;
     }
 
-    const {primaryId} = this.element as unknown as {primaryId: string};
-    const primaryAction = primaryId &&
-        this.element.renderRoot.querySelector<HTMLElement>(`#${primaryId}`);
     if (!primaryAction) {
       throw new Error(
           '`ChipHarness.action` is "primary", but the chip does not have a primary action.');
