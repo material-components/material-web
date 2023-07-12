330c10ca4ee58df4f07d8cf29733fffe43ed3bff
vdegenne <vdegenne@gmail.com>
2023-07-12 14:47:37 +0200

    use CustomEvent instead

--- checkbox/lib/checkbox.ts
+++ checkbox/lib/checkbox.ts
@@ -14,19 +14,7 @@ import {classMap} from 'lit/directives/class-map.js';
 import {ARIAMixinStrict} from '../../internal/aria/aria.js';
 import {requestUpdateOnAriaChange} from '../../internal/aria/delegate.js';
 import {dispatchActivationClick, isActivationClick, redispatchEvent} from '../../internal/controller/events.js';
-import type {MdCheckbox} from '../checkbox.js';
 
-export class CheckboxCheckedEvent extends Event {
-  constructor (public checkbox: MdCheckbox) {
-    super('checked', { bubbles: true, composed: true })
-  }
-}
-
-declare global {
-  interface HTMLElementEventMap {
-    'checked': CheckboxCheckedEvent
-  }
-}
 
 /**
  * A checkbox component.
@@ -124,7 +112,10 @@ export class Checkbox extends LitElement {
       this.prevIndeterminate =
           changed.get('indeterminate') ?? this.indeterminate;
       
-      this.dispatchEvent(new CheckboxCheckedEvent(this))
+      this.dispatchEvent(new CustomEvent('checked', {
+        bubbles: true, composed: true,
+        detail: { checked: this.checked }
+      }))
     }
 
     const shouldAddFormValue = this.checked && !this.indeterminate;
