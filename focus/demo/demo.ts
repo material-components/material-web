f7eff48c665ba5d59756c7d9013bcd058ba41575
Elizabeth Mitchell <lizmitchell@google.com>
2023-07-18 14:34:01 -0700

    fix(focus)!: move --md-focus-ring tokens to host
    BREAKING CHANGE: Set `--md-focus-ring-*` tokens directly on `<md-focus-ring>` elements. Focus rings are exposed with `::part(focus-ring)`.
    
    PiperOrigin-RevId: 549114124

--- focus/demo/demo.ts
+++ focus/demo/demo.ts
@@ -8,25 +8,13 @@ import './index.js';
 import './material-collection.js';
 
 import {KnobTypesToKnobs, MaterialCollection, materialInitsToStoryInits, setUpDemo} from './material-collection.js';
-import {boolInput, cssCustomProperty, Knob, textInput} from './index.js';
+import {boolInput, Knob} from './index.js';
 
 import {stories, StoryKnobs} from './stories.js';
 
 const collection =
     new MaterialCollection<KnobTypesToKnobs<StoryKnobs>>('Focus', [
       new Knob('inward', {ui: boolInput(), defaultValue: false}),
-      new Knob(
-          '--md-focus-ring-width',
-          {ui: textInput(), wiring: cssCustomProperty, defaultValue: '3px'}),
-      new Knob(
-          '--md-focus-ring-active-width',
-          {ui: textInput(), wiring: cssCustomProperty, defaultValue: '8px'}),
-      new Knob(
-          '--md-focus-ring-outward-offset',
-          {ui: textInput(), wiring: cssCustomProperty, defaultValue: '2px'}),
-      new Knob(
-          '--md-focus-ring-inward-offset',
-          {ui: textInput(), wiring: cssCustomProperty, defaultValue: '0px'}),
     ]);
 
 collection.addStories(...materialInitsToStoryInits(stories));
