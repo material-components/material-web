6719e480c33b3aec48d982c89fe9a3e369a1691e
Elizabeth Mitchell <lizmitchell@google.com>
2023-08-18 09:51:28 -0700

    chore(fab): update demo and fix a11y
    PiperOrigin-RevId: 558173479

--- fab/demo/demo.ts
+++ fab/demo/demo.ts
@@ -7,13 +7,13 @@
 import './index.js';
 import './material-collection.js';
 
-import {FabSize, FabVariant} from '@material/web/fab/fab.js';
+import {FabSize} from '@material/web/fab/fab.js';
 import {KnobTypesToKnobs, MaterialCollection, materialInitsToStoryInits, setUpDemo} from './material-collection.js';
 import {boolInput, Knob, selectDropdown, textInput} from './index.js';
 
 import {stories, StoryKnobs} from './stories.js';
 
-const collection = new MaterialCollection<KnobTypesToKnobs<StoryKnobs>>('Fab', [
+const collection = new MaterialCollection<KnobTypesToKnobs<StoryKnobs>>('FAB', [
   new Knob('icon', {defaultValue: 'edit', ui: textInput()}),
   new Knob('label', {defaultValue: '', ui: textInput()}),
   new Knob('lowered', {defaultValue: false, ui: boolInput()}),
@@ -24,17 +24,6 @@ const collection = new MaterialCollection<KnobTypesToKnobs<StoryKnobs>>('Fab', [
         {label: 'medium', value: 'medium'},
         {label: 'small', value: 'small'},
         {label: 'large', value: 'large'},
-      ]
-    })
-  }),
-  new Knob('variant', {
-    defaultValue: 'surface' as FabVariant,
-    ui: selectDropdown<FabVariant>({
-      options: [
-        {label: 'surface', value: 'surface'},
-        {label: 'primary', value: 'primary'},
-        {label: 'secondary', value: 'secondary'},
-        {label: 'tertiary', value: 'tertiary'},
       ]
     })
   }),
--- fab/demo/stories.ts
+++ fab/demo/stories.ts
@@ -8,9 +8,9 @@ import '@material/web/fab/fab.js';
 import '@material/web/icon/icon.js';
 import '@material/web/fab/branded-fab.js';
 
-import {FabSize, FabVariant} from '@material/web/fab/fab.js';
-import {MaterialStoryInit} from './material-collection.js';
-import {css, html} from 'lit';
+import {FabSize} from '@material/web/fab/fab.js';
+import {labelStyles, MaterialStoryInit} from './material-collection.js';
+import {css, html, nothing} from 'lit';
 
 /** Knob types for fab stories. */
 export interface StoryKnobs {
@@ -18,53 +18,100 @@ export interface StoryKnobs {
   label: string;
   lowered: boolean;
   size: FabSize|undefined;
-  variant: FabVariant|undefined;
 }
 
 const styles = css`
-  .fab {
-    padding: 8px;
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 16px;
+  }
+
+  label {
+    flex-direction: column;
+    gap: 16px;
   }
 `;
 
 const standard: MaterialStoryInit<StoryKnobs> = {
-  name: '<md-fab>',
-  styles,
-  render({icon, label, lowered, size, variant}) {
+  name: 'Floating action buttons',
+  styles: [labelStyles, styles],
+  render({icon, label, lowered, size}) {
     return html`
-      <md-fab
-          class="fab"
-          .variant=${variant!}
-          .lowered=${lowered}
-          .label=${label}
-          .size=${size!}>
-        <md-icon slot="icon">${icon}</md-icon>
-      </md-fab>
+      <div class="row">
+        <label>
+          <span aria-hidden="true">Surface</span>
+          <md-fab
+            aria-label=${label ? nothing : 'An example surface FAB'}
+            variant="surface"
+            .lowered=${lowered}
+            .label=${label}
+            .size=${size!}
+          >
+            <md-icon slot="icon">${icon}</md-icon>
+          </md-fab>
+        </label>
+
+        <label>
+          <span aria-hidden="true">Primary</span>
+          <md-fab
+            aria-label=${label ? nothing : 'An example primary FAB'}
+            variant="primary"
+            .lowered=${lowered}
+            .label=${label}
+            .size=${size!}
+          >
+            <md-icon slot="icon">${icon}</md-icon>
+          </md-fab>
+        </label>
+
+        <label>
+          <span aria-hidden="true">Secondary</span>
+          <md-fab
+            aria-label=${label ? nothing : 'An example secondary FAB'}
+            variant="secondary"
+            .lowered=${lowered}
+            .label=${label}
+            .size=${size!}
+          >
+            <md-icon slot="icon">${icon}</md-icon>
+          </md-fab>
+        </label>
+
+        <label>
+          <span aria-hidden="true">Tertiary</span>
+          <md-fab
+            aria-label=${label ? nothing : 'An example tertiary FAB'}
+            variant="tertiary"
+            .lowered=${lowered}
+            .label=${label}
+            .size=${size!}
+          >
+            <md-icon slot="icon">${icon}</md-icon>
+          </md-fab>
+        </label>
+
+        <label>
+          <span aria-hidden="true">Branded</span>
+          <md-branded-fab
+            aria-label=${label ? nothing : 'An example branded FAB'}
+            .lowered=${lowered}
+            .label=${label}
+            .size=${size!}
+          >
+            <svg slot="icon" width="36" height="36" viewBox="0 0 36 36">
+              <path fill="#34A853" d="M16 16v14h4V20z"></path>
+              <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+              <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+              <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+              <path fill="none" d="M0 0h36v36H0z"></path>
+            </svg>
+          </md-branded-fab>
+        </label>
+      </div>
     `;
   }
 };
 
-const branded: MaterialStoryInit<StoryKnobs> = {
-  name: '<md-branded-fab>',
-  styles,
-  render({label, lowered, size}) {
-    return html`
-      <md-branded-fab
-          class="fab"
-          .lowered=${lowered}
-          .label=${label}
-          .size=${size!}>
-        <svg slot="icon" width="36" height="36" viewBox="0 0 36 36">
-          <path fill="#34A853" d="M16 16v14h4V20z"></path>
-          <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
-          <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
-          <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
-          <path fill="none" d="M0 0h36v36H0z"></path>
-        </svg>
-    </md-branded-fab>
-  `;
-  }
-};
-
 /** Checkbox stories. */
-export const stories = [standard, branded];
+export const stories = [standard];
