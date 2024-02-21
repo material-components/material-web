c7e26b5e93b0e088f0491e386c40038e373eae80
Luke Vo <datvm@outlook.com>
2024-02-21 09:09:31 +0700

    Added back the buffer check vs. max value

--- progress/internal/linear-progress.ts
+++ progress/internal/linear-progress.ts
@@ -42,7 +42,7 @@ export class LinearProgress extends Progress {
     // Only display dots when visible - this prevents invisible infinite
     // animation.
     const hideDots =
-      this.indeterminate || !hasBuffer || this.value >= this.max;
+      this.indeterminate || !hasBuffer || bufferValue >= this.max || this.value >= this.max;
     return html`
       <div class="dots" ?hidden=${hideDots}></div>
       <div class="inactive-track" style=${styleMap(dotStyles)}></div>
