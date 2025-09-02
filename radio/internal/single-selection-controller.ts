material-components/material-web

commit 688ab3cf5f12ddbff07407910b8e8e158b3282d7

fix(radio): stack overflow error when rendering many radios
PiperOrigin-RevId: 791255859

radio/internal/single-selection-controller.ts
@@ -84,17 +84,29 @@ export class SingleSelectionController implements ReactiveController {
       this.uncheckSiblings();
     }
 
-    // Update for the newly added host.
-    this.updateTabIndices();
+    // Update siblings after a microtask to allow other synchronous connected
+    // callbacks to settle before triggering additional Lit updates. This avoids
+    // stack overflow issues when too many elements are being rendered and
+    // connected at the same time.
+    queueMicrotask(() => {
+      // Update for the newly added host.
+      this.updateTabIndices();
+    });
   }
 
   hostDisconnected() {
     this.host.removeEventListener('keydown', this.handleKeyDown);
     this.host.removeEventListener('focusin', this.handleFocusIn);
     this.host.removeEventListener('focusout', this.handleFocusOut);
-    // Update for siblings that are still connected.
-    this.updateTabIndices();
-    this.root = null;
+    // Update siblings after a microtask to allow other synchronous disconnected
+    // callbacks to settle before triggering additional Lit updates. This avoids
+    // stack overflow issues when too many elements are being rendered and
+    // connected at the same time.
+    queueMicrotask(() => {
+      // Update for siblings that are still connected.
+      this.updateTabIndices();
+      this.root = null;
+    });
   }
 
   /**

commit adb8d104f2ebc29890b8c578e34a412e8c5c3fc2

fix(radio): move root assignment to mirror hostDisconnected
Fixes a regression caused by 688ab3c where calling hostConnected / hostDisconnected in rapid succession can cause root to become null.

radio/internal/single-selection-controller.ts
@@ -74,7 +74,6 @@ export class SingleSelectionController implements ReactiveController {
   constructor(private readonly host: SingleSelectionElement) {}
 
   hostConnected() {
-    this.root = this.host.getRootNode() as ParentNode;
     this.host.addEventListener('keydown', this.handleKeyDown);
     this.host.addEventListener('focusin', this.handleFocusIn);
     this.host.addEventListener('focusout', this.handleFocusOut);
@@ -90,6 +89,7 @@ export class SingleSelectionController implements ReactiveController {
     // connected at the same time.
     queueMicrotask(() => {
       // Update for the newly added host.
+      this.root = this.host.getRootNode() as ParentNode;
       this.updateTabIndices();
     });
   }