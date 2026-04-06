fb8a05902a82fa8ed5e0bb9480670a1eb9fdc616
Elizabeth Mitchell <lizmitchell@google.com>
2026-04-06 16:32:39 -0700

    chore: generate `*.cssresult.js` for CSS files
    PiperOrigin-RevId: 895557948

--- scripts/css-to-ts.ts
+++ scripts/css-to-ts.ts
@@ -5,13 +5,26 @@
  */
 
 import * as fs from 'fs';
+import * as util from 'util';
 
-const cssFilePath = process.argv[2];
+const {values, positionals} = util.parseArgs({
+  allowPositionals: true,
+  options: {
+    suffix: {
+      type: 'string',
+    },
+  },
+});
+
+const cssFilePath = positionals[0];
 if (!cssFilePath) {
-  throw new Error(`Usage: node scripts/css-to-ts.js <input.css> [output.ts]`);
+  throw new Error(
+    `Usage: node scripts/css-to-ts.js <input.css> [output.ts] [--suffix=<suffix>]`,
+  );
 }
 
-const tsFilePath = process.argv[3] || cssFilePath.replace('.css', '.ts');
+const tsFilePath =
+  positionals[1] || cssFilePath.replace('.css', `${values.suffix || ''}.ts`);
 const cssContent = fs
   .readFileSync(cssFilePath, {encoding: 'utf8'})
   // Remove source map comments since the css is embedded.
@@ -22,7 +35,7 @@ fs.writeFileSync(
   tsFilePath,
   `/**
  * @license
- * Copyright 2024 Google LLC
+ * Copyright ${new Date().getFullYear()} Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
 // Generated stylesheet for ${cssFilePath}.
